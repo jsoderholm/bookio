34cf818b1d0d516b3c880d4933a1fc0903e26ee9
jsoderholm <103559938+jsoderholm@users.noreply.github.com>
2024-05-31 17:48:25 +0200

    Implement Kinde Auth (#7)
    Closes #3

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,13 @@
 import { Hono } from "hono"
 import { serveStatic } from "hono/bun"
 import { logger } from "hono/logger"
-import { testRoute } from "./routes/test"
+import { authRoute } from "./routes/auth"
 
 const app = new Hono()
 
 app.use("*", logger())
 
-const apiRoutes = app.basePath("/api").route("/test", testRoute)
+const apiRoutes = app.basePath("/api").route("/", authRoute)
 
 app.get("*", serveStatic({ root: "./client/dist" }))
 app.get("*", serveStatic({ path: "./client/dist/index.html" }))
