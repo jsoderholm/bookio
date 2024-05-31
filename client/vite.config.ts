66af11cc98c5f60a7fc04f29b8f596caee14e5a4
jsoderholm <soederholm.john@gmail.com>
2024-05-31 22:59:23 +0200

    Removing Hono Plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,11 @@
 import path from "node:path"
-import pages from "@hono/vite-cloudflare-pages"
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    TanStackRouterVite(),
-    pages({
-      entry: ["./src/main.tsx", "../server/index.ts"],
-    }),
-  ],
+  plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
